1ff8bd983ae289af126e227344197bbe98ae2c0c
Debtanu Ganguly <debtanu.july22@gmail.com>
2018-09-17 00:37:47 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -17,8 +17,29 @@ io.sockets.on('connection' , function(socket){
 	console.log('Connected: %s sockets connected' , connections.length);
 
 	//Disconnect
+	socket.on('disconnect',function(data){
+		if(!socket.username) return;
+		users.splice(users.indexOf(socket.username), 1);
+		updateUsernames();
 	connections.slice(connections.indexOf(socket), 1);
 	console.log('Disconnected %s sockets connected', connections.length);
+    });
+    //send message
+    socket.on('send message', function(data){
+    	//console.log(data);
+    	io.sockets.emit('new message', {msg: data, user: socket.username});
+    });
+    //New User
+    socket.on('new User', function(data, callback){
+    	callback(true);
+    	socket.username= data;
+    	users.push(socket.username);
+    	updateUsernames();
+    });
+    function updateUsernames(){
+    	io.sockets.emit('get users', users);
+    }
 });
 
 
+ 
